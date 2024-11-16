20f65891c9c31a340809fc8c437601c729b8e668
Taras Ishchuk <ishchuktaras@gmail.com>
2024-11-16 17:55:32 +0100

    link to the styles edit

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,10 @@
-import { SignUp } from '@clerk/nextjs'
+import { SignUp } from '@clerk/nextjs';
+import "/home/tarik/Dokumenty/e_commerce_militarik/ecommerce_admin/app/globals.css";
 
 export default function Page() {
-  return <SignUp />
+  return (
+  <div className='h-screen flex justify-center items-center'>
+    <SignUp />
+  </div>
+  );
 }
